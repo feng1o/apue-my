da8d67c89e9b156d0b45f392c54873aea0480750
feng1o <liufeng_mail@126.com>
2016-03-31 17:10:48 +0800

    stdctl

---
+++ threadctl/supportsystem.c
@@ -0,0 +1,18 @@
+#include"stdlib.h"
+#include"stdio.h"
+#include"unistd.h"
+#include"sys/types.h"
+
+int main()
+{
+    pid_t id;
+    id = fork();
+    if(id != 0)
+        printf("id != 0 ===%d\n",id);
+    printf("uid = %d, euid =%d\n", getuid(),geteuid());
+    system("ls");
+    system("date");
+    printf("%d\n",system(NULL));
+    printf("nice = %d\n",nice(100000));
+    return 0;
+}
